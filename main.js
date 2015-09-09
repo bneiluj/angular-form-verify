cce99610a8c1d756e94ec885b276fe96b7a32e1f
bneiluj <julien.bouteloup@gmai.com>
2015-09-09 15:09:43 +0100

    make it working with angular 1.4.5

--- main.js
+++ main.js
@@ -1,35 +1,38 @@
-var app = angular.module("app",[]);
+var app = angular.module("app", []);
 
-app.controller("loginCtrl", function ($scope){
+app.controller("loginCtrl", function($scope) {
     'use strict';
 
-    $scope.login = function (form){
+    $scope.login = function(form) {
         var email = form.email.$modelValue,
             password = form.password.$modelValue;
 
+        /**
+         * You can call the service here
+         */
         alert("Logged in");
     }
 });
 
-app.directive("formVerify",function(){
-	'use strict';
+app.directive("formVerify", function() {
+    'use strict';
 
-	return {
-		require: '^form',
-		scope: {
-			submit: '&',
-			submitted: '='
-		},
+    return {
+        require: '^form',
+        scope: {
+            submit: '&',
+            submitted: '='
+        },
 
-		link: function(scope,element,attrs,form){
-			element.on('submit',function(){
-				scope.$apply(function(){
-					if(form.$valid){
-						return scope.submit();
-					}
-					scope.submitted = true;
-				})
-			})
-		}
-	}
+        link: function(scope, element, attrs, form) {
+            element.on('submit', function() {
+                scope.$apply(function() {
+                    if (form.$valid) {
+                        return scope.submit();
+                    }
+                    scope.submitted = true;
+                })
+            })
+        }
+    }
 });
